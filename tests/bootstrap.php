8361239bc9fbe2edfe79c98453fc572fc430fd32
Juan Treminio <jtreminio@gmail.com>
2013-04-02 01:46:28 -0500

    Removed definition

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,4 @@ use jtreminio\Container\Container;
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('jtreminio\Container\Tests', __DIR__);
 
-$pimple = new \Pimple();
-
-$pimple['FooBarDateTime'] = function () {
-    return new \DateTime();
-};
-
-Container::setPimple($pimple);
+Container::setPimple(new \Pimple);
