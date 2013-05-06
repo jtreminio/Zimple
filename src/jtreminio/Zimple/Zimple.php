0e6844b61875d6cbc3b18dba3d367a88961db6b7
Juan Treminio <jtreminio@gmail.com>
2013-05-06 02:12:50 -0500

    Initial v2 commit!

--- src/jtreminio/Zimple/Zimple.php
+++ src/jtreminio/Zimple/Zimple.php
@@ -6,114 +6,121 @@ use \Pimple;
 use \ReflectionClass;
 
 /**
- * This is a wrapper for Pimple that adds several useful features to make it easier to manage your code!
- *
- * This is the non-static version.
+ * This is a wrapper for Pimple that adds some useful features to make it easier to manage code!
  *
  * @package jtreminio/Zimple
  * @author  Juan Treminio <jtreminio@gmail.com>
  */
-class Zimple
+class Zimple extends Pimple
 {
-    /** @var Pimple */
-    private $pimple;
+    protected $lockedValues = array();
 
     /**
-     * Set the Pimple instance
+     * Sets a parameter or an object.
      *
-     * @param Pimple $pimple
+     * Objects must be defined as Closures.
+     *
+     * Allowing any PHP callable leads to difficult to debug problems
+     * as function names (strings) are callable (creating a function with
+     * the same a name as an existing parameter would break your container).
+     *
+     * @param string $id        The unique identifier for the parameter or object
+     * @param mixed  $value     The value of the parameter or a closure to defined an object
+     * @param array $parameters The default parameters to use when calling this object
+     * @return self
      */
-    public function setPimple(Pimple $pimple)
+    public function set($id, $value, array $parameters = array())
     {
-        $this->pimple = $pimple;
+        // We want to keep parameters around if setting this object
+        if (!empty($parameters) && !$this->offsetExists("lock.{$id}")) {
+            $serializedParams = md5(serialize($parameters));
+            $this[$serializedParams] = $parameters;
+        }
+
+        // $value is fqcn and has no parameters
+        if (is_string($value) && empty($serializedParams) && class_exists($value)) {
+            $value = function () use ($value) {
+                return new $value;
+            };
+        }
+
+        // $value is fqcn and has constructor parameters
+        if (is_string($value) && !empty($serializedParams)) {
+            $value = function ($container) use ($value, $serializedParams) {
+                $reflectionClass = new ReflectionClass($value);
+                return $reflectionClass->newInstanceArgs($container[$serializedParams]);
+            };
+        }
+
+        if (!$this->offsetExists("lock.{$id}")) {
+            $this->offsetSet($id, $value);
+        }
+
+        return $this;
     }
 
     /**
-     * Get service from the container
+     * Gets a parameter or an object.
      *
-     * @param string $service    Service name, "\Namespace\Component\UserIdentity"
-     * @param array  $parameters Parameters service may require for constructor
-     * @param bool   $final      Flag to override service when parameters are sent.
+     * @param string $id         The unique identifier for the parameter or object
+     * @param array  $parameters Parameters object may require for constructor.
+     *                           If no parameters passed and the object was previously set, will use pre-defined params
      * @return mixed
      */
-    public function get($service, array $parameters = array(), $final = false)
+    public function get($id, array $parameters = array())
     {
-        $this->pimple->offsetSet('parameters', $parameters);
-
-        $finalServiceName = "{$service}-isFinal";
-
-        if ($final) {
-            $this->pimple->offsetSet($finalServiceName, $service);
+        // If service does not exist, return new object without creating definition in container
+        if (!$this->offsetExists($id)) {
+            return $this->getUndefined($id, $parameters);
         }
 
-        // If service does not exist, add it to Pimple
-        if (!$this->pimple->offsetExists($service)) {
-            $this->createNewService($service);
+        // Only use ReflectionClass if parameters are needed in constructor
+        if (!empty($parameters) && !$this->offsetExists("lock.{$id}")) {
+            $serializedParams = md5(serialize($parameters));
+            $this[$serializedParams] = $parameters;
 
-            return $this->pimple[$service];
+            $reflectionClass = new ReflectionClass($id);
+            return $reflectionClass->newInstanceArgs($this[$serializedParams]);
         }
 
-        // Do this again for services called with new set of parameters
-        if (!empty($parameters) && !$this->pimple->offsetExists($finalServiceName)) {
-            $this->createNewService($service);
-        }
-
-        return $this->pimple[$service];
+        return parent::offsetGet($id);
     }
 
     /**
-     * @param string $serviceName Service name - fully qualified
-     * @param mixed  $service     Value to return
-     * @param bool   $final       Flag to override service when parameters are sent. This is useful when injecting a
-     *                            mocked object from our tests and not allowing our code to override the mock with a
-     *                            new service definition.
+     * Set a value and prevent modification to return value
+     *
+     * @param string $id        The unique identifier for the parameter or object
+     * @param mixed  $value     The value of the parameter or a closure to defined an object
+     * @param array $parameters The default parameters to use when calling this object
+     * @return self
      */
-    public function set($serviceName, $service, $final = false)
+    public function lock($id, $value, array $parameters = array())
     {
-        $this->pimple->offsetUnset($serviceName);
+        $obj = $this->set($id, $value, $parameters);
+        $this->offsetSet("lock.{$id}", true);
 
-        $this->pimple->offsetSet($serviceName, $service);
-
-        if ($final) {
-            $finalServiceName = "{$serviceName}-isFinal";
-            $this->pimple->offsetSet($finalServiceName, 1);
-        }
+        return $obj;
     }
 
     /**
-     * Clear all defined services
+     * Clear all defined
      */
     public function clear()
     {
-        foreach ($this->pimple->keys() as $key) {
-            $this->pimple->offsetUnset($key);
+        foreach ($this->keys() as $key) {
+            $this->offsetUnset($key);
         }
-    }
-
-    /**
-     * Create a new service in Pimple provider
-     *
-     * @param string $service    Service name - fully qualified
-     */
-    private function createNewService($service)
-    {
-        $object = $this->getUndefined($service);
-
-        $this->pimple[$service] = function () use ($object) {
-            return $object;
-        };
     }
 
     /**
      * If the service has not been defined within the container, attempt to instantiate the class directly
      *
      * @param string $fqClassName Fully qualified class name
+     * @param array  $parameters Parameters object may require for constructor
      * @return object
      */
-    private function getUndefined($fqClassName)
+    private function getUndefined($fqClassName, array $parameters)
     {
-        $parameters = $this->pimple->offsetExists('parameters') ? $this->pimple->offsetGet('parameters') : false;
-
         // Only use ReflectionClass if parameters are needed in constructor
         if (!empty($parameters)) {
             $reflectionClass = new ReflectionClass($fqClassName);
--- tests/jtreminio/Zimple/Tests/ZimpleTest.php
+++ tests/jtreminio/Zimple/Tests/ZimpleTest.php
@@ -2,72 +2,26 @@
 
 namespace jtreminio\Zimple\Tests;
 
-use jtreminio\Zimple\Zimple as Container;
+use jtreminio\Zimple\Zimple;
 
 class ZimpleTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var Container */
-    public $container;
+    /** @var Zimple */
+    public $zimple;
 
     public function setUp()
     {
-        $this->container = new Container;
-        $this->container->setPimple(new \Pimple);
+        $this->zimple = new zimple;
     }
 
     public function tearDown()
     {
-        $this->container->clear();
+        $this->zimple->clear();
     }
 
-    /**
-     * Override service container
-     *
-     * @param string $name    Name of service to replace: "\Namespace\Component\UserIdentity"
-     * @param mixed  $service Value to replace service with - usually a PHPUnit mock object
-     * @param bool   $final   Whether to mark service as final, prevent overrides
-     * @return self
-     */
-    protected final function setService($name, $service, $final = true)
+    public function testGetInstantiatesNewObjectWithoutParameters()
     {
-        $this->container->set($name, $service, $final);
-
-        return $this;
-    }
-
-    public function testGetReturnsService()
-    {
-        $uniqId = uniqid();
-        $string = 'it works!';
-
-        $this->setService($uniqId, $string);
-
-        $this->assertEquals(
-            $string,
-            $this->container->get($uniqId),
-            "Expected \$this->container->get() to return {$string}"
-        );
-    }
-
-    public function testGetReturnsServiceWithConstructorInjection()
-    {
-        $date = '2000-01-01';
-
-        /** @var $date \DateTime */
-        $date = $this->container->get('\DateTime', array($date));
-
-        $expectedResult = '946706400';
-
-        $this->assertEquals(
-            $expectedResult,
-            $date->getTimestamp(),
-            'Returned timestamp did not match expected'
-        );
-    }
-
-    public function testGetReturnsInstantiatedObjectWhenNotADefinedService()
-    {
-        $stdClass = $this->container->get('\stdClass');
+        $stdClass = $this->zimple->get('\stdClass');
 
         $this->assertInstanceOf(
             '\stdClass',
@@ -76,10 +30,12 @@ class ZimpleTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testGetReturnsInstantiatedObjectWithConstructorParametersWhenNotADefinedService()
+    public function testGetInstantiatesNewObjectWithParameters()
     {
+        $date = '2000-01-01';
+
         /** @var $date \DateTime */
-        $date = $this->container->get('\DateTime', array('2000-01-01'));
+        $date = $this->zimple->get('\DateTime', array($date));
 
         $expectedResult = '946706400';
 
@@ -90,16 +46,16 @@ class ZimpleTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testGetReturnsNewObjectsWhenParametersAreDifferent()
+    public function testGetReturnsNewObjectsOnSubsequentCalls()
     {
         /** @var $date1 \DateTime */
-        $date1 = $this->container->get('\DateTime', array('2000-01-01'));
+        $date1 = $this->zimple->get('\DateTime', array('2000-01-01'));
 
         /** @var $date2 \DateTime */
-        $date2 = $this->container->get('\DateTime', array('2005-01-01'));
+        $date2 = $this->zimple->get('\DateTime', array('2005-01-01'));
 
         /** @var $date3 \DateTime */
-        $date3 = $this->container->get('\DateTime', array('2013-01-01'));
+        $date3 = $this->zimple->get('\DateTime', array('2013-01-01'));
 
         $expectedResult1 = '946706400';
         $expectedResult2 = '1104559200';
@@ -124,17 +80,105 @@ class ZimpleTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testSetOverridesExistingServiceWithNew()
+    public function testTakesObjectAndStringAsValue()
+    {
+        $this->zimple->set('\DateTime', new \DateTime);
+        $this->zimple->set('\DateTime2', '\DateTime');
+
+        $this->assertInstanceOf(
+            '\DateTime',
+            $this->zimple->get('\DateTime'),
+            'Expected \DateTime definition to return a DateTime object'
+        );
+
+        $this->assertInstanceOf(
+            '\DateTime',
+            $this->zimple->get('\DateTime2'),
+            'Expected \DateTime2 definition to return a DateTime object'
+        );
+    }
+
+    public function testGetReturnsPreviouslySetParameteredObjectWithNoGetParameters()
+    {
+        $this->zimple->set('\DateTime', '\DateTime', array('2000-01-01'));
+        $this->zimple->set('foobar', '\DateTime', array('2020-01-01'));
+
+        $date1 = $this->zimple->get('\DateTime');
+        $date2 = $this->zimple->get('foobar');
+
+        $this->assertEquals(
+            '946706400',
+            $date1->getTimestamp(),
+            'Expected \DateTime to match previously defined'
+        );
+
+        $this->assertEquals(
+            '1577858400',
+            $date2->getTimestamp(),
+            'Expected foobar to match previously defined'
+        );
+    }
+
+    public function testGetWithParametersAllowsOverrideOfPreviouslySetDefinition()
+    {
+        $this->zimple->set('\DateTime', '\DateTime', array('2000-01-01'));
+
+        $date = $this->zimple->get('\DateTime');
+        $dateOverride = $this->zimple->get('\DateTime', array('2020-01-01'));
+
+        $this->assertEquals(
+            '946706400',
+            $date->getTimestamp(),
+            'Expected \DateTime to match previously defined'
+        );
+
+        $this->assertEquals(
+            '1577858400',
+            $dateOverride->getTimestamp(),
+            'Expected get overrides existing definition with new parameters'
+        );
+    }
+
+    public function testGetIgnoresParametersWhenDefinitionLocked()
+    {
+        $this->zimple->lock('date1', new \DateTime('2000-01-01'));
+        $this->zimple->lock('date2', new \DateTime('2001-01-01'));
+        $this->zimple->lock('date3', '\DateTime', array('2002-01-01'));
+
+        $this->zimple->set('date1', new \DateTime('2000-01-01'));
+        $this->zimple->set('date3', new \DateTime('2000-01-01'));
+
+        $date1 = $this->zimple->get('date1');
+        $date2 = $this->zimple->get('date2', array('2005-01-01'));
+        $date3 = $this->zimple->get('date3');
+
+        $this->assertEquals(
+            '946706400',
+            $date1->getTimestamp()
+        );
+
+        $this->assertEquals(
+            '978328800',
+            $date2->getTimestamp()
+        );
+
+        $this->assertEquals(
+            '1009864800',
+            $date3->getTimestamp()
+        );
+    }
+
+    public function testSetReturnsDefinedValueAfterPreviousUninstantiatedCall()
     {
         /** @var $date1 \DateTime */
-        $date1 = $this->container->get('\DateTime', array('2000-01-01'));
+        $date1 = $this->zimple->get('\DateTime', array('2000-01-01'));
 
         $dateOverride = new \DateTime();
 
         /** @var $date2 \DateTime */
-        $this->setService('\DateTime', $dateOverride);
+        $this->zimple->set('\DateTime', $dateOverride);
 
-        $date2 = $this->container->get('\DateTime');
+        $date2 = $this->zimple->get('\DateTime');
 
         $expectedResult1 = '946706400';
 
@@ -150,58 +194,45 @@ class ZimpleTest extends \PHPUnit_Framework_TestCase
             'Returned timestamp did not match expected for date2'
         );
     }
-
-    public function testSetPreventsOverrideOfExistingServiceWhenFlagged()
+    public function testGetReturnsSameObjectWhenSetPassedLiteral()
     {
-        /** @var $date1 \DateTime */
-        $date1 = $this->container->get('\DateTime', array('2000-01-01'));
-
-        $dateOverride = new \DateTime('2005-01-01');
-
-        /** @var $date2 \DateTime */
-        $this->container->set('\DateTime', $dateOverride, true);
-
-        $date2 = $this->container->get('\DateTime');
-
-        $date3 = $this->container->get('\DateTime', array('2000-01-01'));
+        $this->zimple->set('unique', uniqid());
+        $uniqid1 = $this->zimple->get('unique');
+        $uniqid2 = $this->zimple->get('unique');
 
         $this->assertSame(
-            $date2,
-            $date3,
-            'Expecting ::set() to prevent overriding existing service when flagged to prevent overrides'
+            $uniqid1,
+            $uniqid2,
+            'Expected ::get() to return same value on multiple calls'
         );
     }
 
-    public function testGetReturnsPredefinedServiceFromPimple()
+    public function testSetAcceptsObjectsAndClosures()
     {
-        $this->container->clear();
+        $this->zimple->set('DateTime1', new \DateTime('2013-05-06'));
 
-        $pimple = new \Pimple;
+        $this->zimple->set('DateTime2', function () {
+            return new \DateTime('2005-01-01');
+        });
 
-        $pimple['FooBarDateTime'] = function () {
-            return new \DateTime();
-        };
+        /** @var \DateTime $date1 */
+        $date1 = $this->zimple->get('DateTime1');
+        /** @var \DateTime $date2 */
+        $date2 = $this->zimple->get('DateTime2');
 
-        $this->container->setPimple($pimple);
+        $expectedResult1 = '1367816400';
+        $expectedResult2 = '1104559200';
 
-        $this->assertInstanceOf(
-            '\DateTime',
-            $this->container->get('FooBarDateTime'),
-            'Expecting ::get() to return a pre-defined Pimple service'
+        $this->assertEquals(
+            $expectedResult1,
+            $date1->getTimestamp(),
+            'Returned timestamp did not match expected for date1'
         );
-    }
 
-    public function testGetReturnsSameInstanceWhenDefinedAsFinal()
-    {
-        /** @var $date1 \DateTime */
-        $date1 = $this->container->get('\DateTime', array('2000-01-01'), true);
-
-        $date2 = $this->container->get('\DateTime', array('2011-11-11'));
-
-        $this->assertSame(
-            $date1,
-            $date2,
-            'Expecting ::get() to return the same instance of \DateTime when final flag is set to true'
+        $this->assertEquals(
+            $expectedResult2,
+            $date2->getTimestamp(),
+            'Returned timestamp did not match expected for date2'
         );
     }
 }
